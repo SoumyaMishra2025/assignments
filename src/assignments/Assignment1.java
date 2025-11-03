e7fb8f9641d31e4404c660422d97c7cdddde6553
Soumya Mishra <s2021.mish@gmail.com>
2025-11-03 23:34:41 +0530

    Assignment1 - Data types

---
+++ src/assignments/Assignment1.java
@@ -0,0 +1,105 @@
+package assignments;
+
+public class Assignment1 {
+	
+
+	public static void main(String[] args) {
+		
+		float temperature = 25.5f;		
+		System.out.println("Temperature of Guwahti now is: "+ temperature + " degree Celcius" );
+		
+		boolean orderPlaced = true;
+		if(orderPlaced) {
+			System.out.println("The order has been placed:" + orderPlaced);
+		}
+		else {
+			System.out.println("The order has not been placed:" + orderPlaced);
+		}
+		
+		
+		  int mobileNumber = 1234567890;
+		  System.out.println("Person's phone number:"+ mobileNumber);
+		 
+		
+		double accountBalance =1000.50;
+		System.out.println("Amount of money in a customer's bank account is: " + accountBalance + " $.");
+		
+		String eMail= "john.doe@example.com";
+		System.out.println("PJohn's email address is: "+ eMail);
+		
+		
+		String city = "Mumbai";
+		double latitude = 37.7749;
+		double longitude = -122.4194;		
+		System.out.println(city + " is situated at a Latitude & longitude of "+ latitude +" and "+ longitude +" respectively.");
+		
+	
+		
+		boolean maritalStatus=true;
+			if(maritalStatus) {
+				System.out.println("Person is married:" + true );
+			}
+			else{
+				System.out.println("Person is married:" + false );
+			}
+			
+//			Person's occupation: "Software Engineer"
+			String occupation = "Software Engineer";
+			System.out.println("Aman work as an "+ occupation + " in Infosys");
+			
+//			Person's favourite colour: "Blue"
+			String color = "Blue";
+			System.out.println("Aman's favourite color is "+ color);
+			
+//			Current year: 2023
+			int year = 2023;
+			System.out.println("Aman graduated in the year: "+ year);
+			
+//			Number of followers on a social media platform: 1,000,000
+			int followers = 1000000;
+			System.out.println("Aman has "+followers + " followers on Instagram");
+			
+
+//			Rating of a movie: 7.5
+			float rating= 7.5f;
+			System.out.println("IMDb rating of movie Dune is " + rating);
+
+//			Person's blood type: 'A'
+			char bloodType = 'A';
+			System.out.println("Blood type of Aman is "+ bloodType);
+
+//			Title of a book: "To Kill a Mockingbird"
+			String bookTitle = "To Kill a Mockingbird";
+			System.out.println("Aman is reading the book "+ bookTitle);
+			
+//			Number of employees in a company: 500
+			int employees = 500;
+			System.out.println("The company has total " +employees+ " employees.");
+
+//			Time of an event: 2:30 PM
+			int time1 = 2;
+			int time2 =30;
+			System.out.println("Aman has an appointment at "+ time1+":"+time2+ " PM.");
+
+//			Name of a country: "United States"
+			String country="United States";
+			System.out.println("Aman lives in "+ country);
+						
+
+//			Person's eye color: "Brown"
+			String eyeColor = "Brown";
+			System.out.println("Aman has "+eyeColor+" eyes.");
+
+//			Person's birthplace: "New York City"
+			String birthPlace="New York City";
+			System.out.println("Birth place of Aman is : "+birthPlace);
+
+//			Distance between two cities: 200.5
+			float distance = 200.5f;
+			System.out.println("Bangalore is "+ distance + " KMs away from Hydrabad.");
+		
+		
+
+	}
+
+}
---
+++ src/language/basics/DebugTheProgram.java
@@ -0,0 +1,17 @@
+package language.basics;
+
+public class DebugTheProgram {
+
+	public static void main(String[] args) {
+		int i=1;
+		System.out.println("Step 1: Program status");
+		System.out.println("Step 2: Preparing to execute next step");
+		System.out.println("Step 3: Performing calculation");
+		int j = i-1;
+		System.out.println(i/j);
+		System.out.println("Step 5: Program execution completed");
+		
+
+	}
+
+}
